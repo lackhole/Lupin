16f7d318ba531828d7a70861882c22cfb04b7d6e
Tony <cosgenio@gmail.com>
2021-04-26 15:02:27 +0900

    add more examples

--- main.cpp
+++ main.cpp
@@ -10,11 +10,24 @@ struct hidden {
   int func() const &{ return x; }
   int sum(int a, int b) { return a + b; }
 
+  void rrefOnly() && {}
+
   static int sta() { return y; }
   static int y;
 
+  virtual int vf() { return 1; }
 };
 int hidden::y = 12345;
+
+struct base {
+ private:
+  virtual void vf() { std::cout << "base\n"; }
+};
+
+struct derived : public base {
+ private:
+  void vf() override { std::cout << "derived\n"; }
+};
 
 ACCESS_CREATE_TAG(tag_hidden_x, hidden, x);
 ACCESS_CREATE_UNIQUE_TAG(hidden, x);
@@ -24,56 +37,82 @@ ACCESS_CREATE_TAG(tag_hidden_func, hidden, func);
 ACCESS_CREATE_TAG(tag_hidden_sum, hidden, sum);
 ACCESS_CREATE_TAG(tag_hidden_sta, hidden, sta);
 ACCESS_CREATE_TAG(tag_hidden_y, hidden, y);
+ACCESS_CREATE_TAG(tag_hidden_rref, hidden, rrefOnly);
+
+ACCESS_CREATE_UNIQUE_TAG(base, vf);
 
 int main() {
-  std::cout << std::boolalpha;
 
   hidden h;
 
+  // can read and write private member
   std::cout << access::get<tag_hidden_x>(h) << std::endl;
-  std::cout << access::get<ACCESS_GET_UNIQUE_TAG(hidden, x)>(h) << std::endl;
-
   access::get<tag_hidden_x>(h) = 100;
   std::cout << access::get<tag_hidden_x>(h) << std::endl;
-  access::get<ACCESS_GET_UNIQUE_TAG(hidden, x)>(h) = 200;
-  std::cout << access::get<ACCESS_GET_UNIQUE_TAG(hidden, x)>(h) << std::endl;
-
-  std::cout << access::get<tag_hidden_y>() << std::endl;
-  access::get<tag_hidden_y>() = -123;
-  std::cout << access::get<tag_hidden_y>() << std::endl;
-
-  access::call<tag_hidden_func>(h);
-  (h.*access::get<tag_hidden_func>(h))();
-
-  access::call<tag_hidden_sum>(h, 1, 2);
-
-  access::get<tag_hidden_sta>()();
-  access::call<tag_hidden_sta>();
 
   std::cout << access::get<tag_hidden_str>(h) << std::endl;
   access::get<tag_hidden_str>(std::move(h));
   std::cout << access::get<tag_hidden_str>(h) << std::endl;
 
+  // using unnamed(unique) tag
+  std::cout << access::get<ACCESS_GET_UNIQUE_TAG(hidden, x)>(h) << std::endl;
+  access::get<ACCESS_GET_UNIQUE_TAG(hidden, x)>(h) = 200;
+  std::cout << access::get<ACCESS_GET_UNIQUE_TAG(hidden, x)>(h) << std::endl;
+
+  // can read and write static private member
+  std::cout << access::get<tag_hidden_y>() << std::endl;
+  access::get<tag_hidden_y>() = -123;
+  std::cout << access::get<tag_hidden_y>() << std::endl;
+
+  // can call private member function
+  access::call<tag_hidden_func>(h);
+  (h.*access::get<tag_hidden_func>(h))();
+
+  // value types overloads
+  access::call<tag_hidden_sum>(h, 1, 2);
+  access::call<tag_hidden_sum>(std::move(h), 1, 2);
+//  access::call<tag_hidden_rref>(h);
+  access::call<tag_hidden_rref>(hidden());
+
+  // can call private static member function
+  access::call<tag_hidden_sta>();
+  access::get<tag_hidden_sta>()();
+//  access::call<tag_hidden_sta>(h);
+
+
   const hidden h2;
+
+  // const overload is read-only
   access::get<tag_hidden_x>(h2);
+//  access::get<tag_hidden_x>(h2) = 132;
 
   access::call<tag_hidden_func>(h2);
-  (h.*access::get<tag_hidden_func>(h2))();
+  (h2.*access::get<tag_hidden_func>(h2))();
 
-  std::cout << access::get<tag_hidden_sta>()() << std::endl;
-  std::cout << access::call<tag_hidden_sta>() << std::endl;
-
+  // Note that access::Type_t returns actual type, while access::get returns it's reference
   static_assert(std::is_same<access::Type_t<tag_hidden_x>, int>::value, "");
-  static_assert(std::is_same<access::Type_t<tag_hidden_y>, int>::value, "");
+  static_assert(std::is_same<decltype(access::get<tag_hidden_x>(h)), int&>::value, "");
 
-  // Note that the below two type is different. Choose your own way.
-  static_assert(std::is_same<access::Type_t<tag_hidden_func>, int () const&>::value, "");                                           // function type
+  // Same applies to the static member variables
+  static_assert(std::is_same<access::Type_t<tag_hidden_y>, int>::value, "");
+  static_assert(std::is_same<decltype(access::get<tag_hidden_y>()), int&>::value, "");
+
+  // Note that type of function is different too
+  static_assert(std::is_same<access::Type_t<tag_hidden_func>, int() const&>::value, "");                                            // function type
   static_assert(std::is_same<decltype(access::get<tag_hidden_func>(std::declval<hidden>())), int (hidden::*)() const&>::value, ""); // function pointer type
 
-  // Note that these three are different too
-  static_assert(std::is_same<access::Type_t<tag_hidden_sta>, int()>::value, "");
-  static_assert(std::is_same<decltype(access::get<tag_hidden_sta>()), int(&)()>::value, "");
-  static_assert(std::is_same<std::decay_t<decltype(access::get<tag_hidden_sta>())>, int(*)()>::value, "");
+  // Same applies to the static member function
+  static_assert(std::is_same<access::Type_t<tag_hidden_sta>, int()>::value, "");              // function type
+  static_assert(std::is_same<decltype(access::get<tag_hidden_sta>()), int(*)()>::value, "");  // function pointer type
+
+
+  // private virtual function can be called
+  base b;
+  derived d;
+
+  access::call<ACCESS_GET_UNIQUE_TAG(base, vf)>(b);
+  access::call<ACCESS_GET_UNIQUE_TAG(base, vf)>(d);
+  access::call<ACCESS_GET_UNIQUE_TAG(base, vf)>(static_cast<base &>(d));
 
   return 0;
 }
